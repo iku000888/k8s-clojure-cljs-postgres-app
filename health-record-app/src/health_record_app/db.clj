e0091b9c9ea2c5a593b050f1d47a45f8665b2848
iku000888 <kanumaiku@gmail.com>
2023-06-01 19:57:30 -0700

    schema setup

--- health-record-app/src/health_record_app/db.clj
+++ health-record-app/src/health_record_app/db.clj
@@ -1,12 +1,35 @@
 (ns health-record-app.db
   (:require [next.jdbc :as jdbc]
             [next.jdbc.sql :as sql]
-            [next.jdbc.connection :as connection])
+            [next.jdbc.connection :as connection]
+            [clojure.string :as str])
   (:import (com.zaxxer.hikari HikariDataSource)))
 
+(defn setup-schema [db]
+  (with-open [c (jdbc/get-connection db)]
+    (when-not (seq (sql/query c
+                              ["SELECT 1 FROM pg_type WHERE typname = 'gender_type'"]))
+      (jdbc/execute! c
+                     ["CREATE TYPE gender_type AS ENUM ('Male', 'Female', 'Other');"]))
+    (when-not (seq (sql/query c
+                              ["SELECT 1 FROM information_schema.tables WHERE table_name = 'patients'"]))
+      (jdbc/execute! c
+                     [(str/join
+                       "\n"
+                       ["CREATE TABLE patients ("
+                        "patient_id SERIAL PRIMARY KEY,"
+                        "name VARCHAR(255) NOT NULL,"
+                        "gender gender_type NOT NULL,"
+                        "date_of_birth DATE NOT NULL,"
+                        "address VARCHAR(255),"
+                        "phone_number VARCHAR(20)"
+                        ")"])]))))
+
 (defn pool [config]
-  (connection/->pool com.zaxxer.hikari.HikariDataSource
-                     {:jdbcUrl (connection/jdbc-url config)}))
+  (let [pool (connection/->pool com.zaxxer.hikari.HikariDataSource
+                                {:jdbcUrl (connection/jdbc-url config)})]
+    (setup-schema pool)
+    pool))
 
 (defn pool.stop [pool]
   (.close pool))
