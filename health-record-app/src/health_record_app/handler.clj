51dec631b8586614abb804486a3fde88ac800553
iku000888 <kanumaiku@gmail.com>
2023-06-02 19:08:38 -0700

    update endpoint

--- health-record-app/src/health_record_app/handler.clj
+++ health-record-app/src/health_record_app/handler.clj
@@ -2,12 +2,22 @@
   (:require [health-record-app.sql :as sql]
             [next.jdbc :as jdbc]))
 
+(defn ->sql-patient [{:keys [gender date_of_birth] :as patient}]
+  (cond-> patient
+    gender (update :gender next.jdbc.types/as-other)
+    date_of_birth (update :date_of_birth #(java.time.LocalDate/parse %))))
+
 (defn patients.add [db]
   (fn add-patient [req]
     (with-open [c (jdbc/get-connection db)]
-      (sql/add-patient c (-> (:body req)
-                             (update :gender next.jdbc.types/as-other)
-                             (update :date_of_birth #(java.time.LocalDate/parse %)))))))
+      (sql/add-patient c (->sql-patient (:body req))))))
+
+(defn patients.update [db]
+  (fn update-patient [req]
+    (with-open [c (jdbc/get-connection db)]
+      (sql/update-patient c
+                          (:patient-id (:route-params req))
+                          (->sql-patient (:body req))))))
 
 (defn patient.delete [db]
   (fn delete-patient [req]
