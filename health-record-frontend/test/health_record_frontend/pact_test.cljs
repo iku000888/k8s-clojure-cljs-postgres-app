f8dadf915f81bdc55a0580bb38f3174812c0ff31
iku000888 <kanumaiku@gmail.com>
2023-06-07 03:10:53 -0700

    refactor + add delete pact UI test

--- health-record-frontend/test/health_record_frontend/pact_test.cljs
+++ health-record-frontend/test/health_record_frontend/pact_test.cljs
@@ -8,86 +8,86 @@
             [cljs.core.async :refer [go <!]]
             [cljs.core.async.interop :refer-macros [<p!]]))
 
+(defn new-patient []
+  (go
+    (t/testing "New patient flow sends compliant POST request"
+      (<p! (.click userEvent
+                   (.getByText rt/screen "New Patient")))
+      (<p! (rt/waitFor (fn [] (.getByLabelText rt/screen "Name"))
+                       #js {:timeout 10000}))
+      (<p! (.selectOptions userEvent
+                           (.getByLabelText rt/screen "Gender")
+                           "Female"))
+      (<p! (.type userEvent
+                  (.getByLabelText rt/screen "Name")
+                  "Jimmy Jon"))
+      (<p! (.type userEvent
+                  (.getByLabelText rt/screen "Date of Birth")
+                  "2000-03-27"))
+      (<p! (.type userEvent
+                  (.getByLabelText rt/screen "Address")
+                  "123 Block B, CA"))
+      (<p! (.type userEvent
+                  (.getByLabelText rt/screen "Phone number (xxx xxx xxxx)")
+                  "333 444 9999"))
+      (<p! (.click userEvent
+                   (.getByText rt/screen "Submit")))
+      (t/is (<p! (rt/waitFor (fn [] (.getByText rt/screen "Request Success"))
+                             #js {:timeout 10000})))
+      (<p! (.click userEvent
+                   (.getByText rt/screen "Dismiss"))))))
+
+(defn update-patient []
+  (go
+    (t/testing "Edit patient flow sends compliant PUT request"
+      (<p! (.click userEvent
+                   (.getByText rt/screen "Edit")))
+      (<p! (rt/waitFor (fn [] (.getByLabelText rt/screen "Name"))
+                       {:timeout 10000}))
+      (<p! (.selectOptions userEvent
+                           (.getByLabelText rt/screen "Gender")
+                           "Male"))
+      (<p! (.clear userEvent
+                   (.getByLabelText rt/screen "Name")))
+      (<p! (.type userEvent
+                  (.getByLabelText rt/screen "Name")
+                  "Limmy Lon"))
+      (<p! (.clear userEvent
+                   (.getByLabelText rt/screen "Date of Birth")))
+      (<p! (.type userEvent
+                  (.getByLabelText rt/screen "Date of Birth")
+                  "2001-03-27"))
+      (<p! (.clear userEvent
+                   (.getByLabelText rt/screen "Address")))
+      (<p! (.type userEvent
+                  (.getByLabelText rt/screen "Address")
+                  "123 Block C, CA"))
+      (<p! (.clear userEvent
+                   (.getByLabelText rt/screen "Phone number (xxx xxx xxxx)")))
+      (<p! (.type userEvent
+                  (.getByLabelText rt/screen "Phone number (xxx xxx xxxx)")
+                  "333 444 9998"))
+      (<p! (.click userEvent
+                   (.getByText rt/screen "Submit")))
+      (t/is (<p! (rt/waitFor (fn [] (.getByText rt/screen "Request Success"))
+                             #js {:timeout 20000})))
+      (<p! (.click userEvent
+                   (.getByText rt/screen "Dismiss"))))))
+
 (t/deftest patient-pact-test
   (rt/render ($ page/app "foo"))
   (async done
          (go
-           (t/testing "New patient flow sends compliant POST request"
+           (<! (new-patient))
+           (<! (update-patient))
+
+           (t/testing "Delete patient flow sends compliant DELETE request"
              (<p! (.click userEvent
-                          (.getByText rt/screen "New Patient")))
-             (<p! (rt/waitFor (fn [] (.getByLabelText rt/screen "Name"))
-                              #js {:timeout 10000}))
-             (<p! (.selectOptions userEvent
-                                  (.getByLabelText rt/screen "Gender")
-                                  "Female"))
-             (<p! (.type userEvent
-                         (.getByLabelText rt/screen "Name")
-                         "Jimmy Jon"))
-             (<p! (.type userEvent
-                         (.getByLabelText rt/screen "Date of Birth")
-                         "2000-03-27"))
-             (<p! (.type userEvent
-                         (.getByLabelText rt/screen "Address")
-                         "123 Block B, CA"))
-             (<p! (.type userEvent
-                         (.getByLabelText rt/screen "Phone number (xxx xxx xxxx)")
-                         "333 444 9999"))
+                          (js/document.querySelector "#check-all")))
              (<p! (.click userEvent
-                          (.getByText rt/screen "Submit")))
+                          (.getByText rt/screen "Delete Selected")))
              (t/is (<p! (rt/waitFor (fn [] (.getByText rt/screen "Request Success"))
-                                    #js {:timeout 10000})))
-             (<p! (.click userEvent
-                          (.getByText rt/screen "Dismiss"))))
-
-           (t/testing "Edit patient flow sends compliant PUT request"
-             (<p! (.click userEvent
-                          (.getByText rt/screen "Edit")))
-             (<p! (rt/waitFor (fn [] (.getByLabelText rt/screen "Name"))
-                              {:timeout 10000}))
-             (<p! (.selectOptions userEvent
-                                  (.getByLabelText rt/screen "Gender")
-                                  "Male"))
-             (<p! (.clear userEvent
-                          (.getByLabelText rt/screen "Name")))
-             (<p! (.type userEvent
-                         (.getByLabelText rt/screen "Name")
-                         "Limmy Lon"))
-             (<p! (.clear userEvent
-                          (.getByLabelText rt/screen "Date of Birth")))
-             (<p! (.type userEvent
-                         (.getByLabelText rt/screen "Date of Birth")
-                         "2001-03-27"))
-             (<p! (.clear userEvent
-                          (.getByLabelText rt/screen "Address")))
-             (<p! (.type userEvent
-                         (.getByLabelText rt/screen "Address")
-                         "123 Block C, CA"))
-             (<p! (.clear userEvent
-                          (.getByLabelText rt/screen "Phone number (xxx xxx xxxx)")))
-             (<p! (.type userEvent
-                         (.getByLabelText rt/screen "Phone number (xxx xxx xxxx)")
-                         "333 444 9998"))
-             (<p! (.click userEvent
-                          (.getByText rt/screen "Submit")))
-             (t/is (<p! (rt/waitFor (fn [] (.getByText rt/screen "Request Success"))
-                                    #js {:timeout 20000})))
-             (<p! (.click userEvent
-                          (.getByText rt/screen "Dismiss"))))
-           ;;(<! (new-patient))
-           #_(<p! (update-patient))
-
-
-           #_(t/testing "Delete patient flow sends compliant DELETE request"
-               (<p! (.click userEvent
-                            (js/document.querySelector "#check-all")))
-               )
-           #_(js/alert "break")
+                                    #js {:timeout 20000}))))
 
            (done)
-           (rt/cleanup)))
-
-
-
-
-
-  )
+           (rt/cleanup))))
